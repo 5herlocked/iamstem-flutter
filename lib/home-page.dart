d873637f2f01af88cf44666e94bb1c9577228d24
Shardul Vaidya <cam.v737@gmail.com>
2021-05-17 17:02:15 -0400

    Table Calendar updated to better reflect API choices

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:i_am_stem/data/event.dart';
 import 'package:i_am_stem/navigation-drawer.dart';
@@ -13,7 +15,7 @@ class _HomePageState extends State<HomePage> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  List<Event>? _eventList;
+  LinkedHashMap<DateTime, List<Event>>? _eventList;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,42 +24,52 @@ class _HomePageState extends State<HomePage> {
       body: Stack(
         children: <Widget>[
           TableCalendar(
-              focusedDay: _focusedDay,
-              // TODO: Change date limits as necessary
-              // by default shows events from a week ago
-              firstDay: _focusedDay.subtract(Duration(days: 7)),
-              // at most shows events from a month later
-              lastDay: _focusedDay.add(Duration(days: 31))
+            focusedDay: _focusedDay,
+            // TODO: Change date limits as necessary
+            // by default shows events from a week ago
+            firstDay: _focusedDay.subtract(Duration(days: 7)),
+            // at most shows events from a month later
+            lastDay: _focusedDay.add(Duration(days: 31)),
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) => _focusedDay = focusedDay,
           ),
           Spacer(
             flex: 5,
           ),
           ListView.builder(
             itemCount: _eventList?.length,
-            itemBuilder: _generateListEvents,
+            itemBuilder: _getEventsForFocusedDay,
           ),
         ],
       ),
     );
   }
 
-  Widget _generateListEvents(BuildContext context, int index) {
-    // TODO: Fill _generateListEvents from DataStore
-    UnimplementedError("Please implement _generateListEvents");
+  List<Event> _getEventList() {
+    // TODO: Call method in Event which fetches all the data
+    return <Event>[];
+  }
+
+  List<Event>? _getEventsForDay(DateTime day) {
+    return _eventList![day];
+  }
+
+  Widget _getEventsForFocusedDay(BuildContext context, int index) {
     return Card(
       child: Center(
         child: Text(
-          "${_eventList![index].name}",
+          "${_eventList![_focusedDay]![index].name}",
           style: Decorations.cardText,
         ),
       ),
     );
-  }
-
-  List<Event> _getEventsForDay(DateTime day) {
-    // TODO: Fill _getEventsForDay from DataStore
-    UnimplementedError("Please implement _getEventsForDay");
-    return [];
   }
 
   PreferredSizeWidget _makeAppBar() {
@@ -65,4 +77,6 @@ class _HomePageState extends State<HomePage> {
       title: Text("Current Events"),
     );
   }
+
+  void _daySelected(DateTime selectedDay, DateTime focusedDay) {}
 }
